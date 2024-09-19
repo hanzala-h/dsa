6a3ce7f417da58db7fef42c13baafa84b5717202
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2024-09-19 11:21:29 +0500

    LinkedList with append method

---
+++ LinkedList.py
@@ -0,0 +1,29 @@
+from Node import Node
+
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value) -> bool:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def __str__(self) -> str:
+        string = ""
+        temp = self.head
+        while temp is not None:
+            string += f"{temp.value} "
+            temp = temp.next
+
+        return string
---
+++ main.py
@@ -0,0 +1,9 @@
+from LinkedList import LinkedList
+
+ll = LinkedList(4)
+
+ll.append(5)
+
+ll.append(6)
+
+print(ll)
