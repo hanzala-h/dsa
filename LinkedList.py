1982d723061cdb7f91d9d443a535fa58a7116389
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2024-09-20 00:39:02 +0500

    feat: prepend method added to LinkedList

--- LinkedList.py
+++ LinkedList.py
@@ -41,6 +41,17 @@ class LinkedList:
 
         return temp.value
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1
+        return True
+
     def __str__(self) -> str:
         if not self.head:
             return "Empty list"
--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from LinkedList import LinkedList
 
 ll = LinkedList(4)
 
+ll.pop()
+
+ll.prepend(6)
+
 print(ll)
